17e79a83a20c7b01041248485205c9b265fa9381
element <72583230+wangxuan66@users.noreply.github.com>
2023-05-22 09:14:01 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Fa镜头视野计算
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TBDistance = new System.Windows.Forms.TextBox();
+            this.TBField = new System.Windows.Forms.TextBox();
+            this.comboBoxCameraTargetSurface = new System.Windows.Forms.ComboBox();
+            this.comboBoxFaLens = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(48, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "相机靶面:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(48, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fa 镜头:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(49, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "物   距:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(49, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "视  野:";
+            // 
+            // TBDistance
+            // 
+            this.TBDistance.Location = new System.Drawing.Point(153, 105);
+            this.TBDistance.Name = "TBDistance";
+            this.TBDistance.Size = new System.Drawing.Size(170, 25);
+            this.TBDistance.TabIndex = 4;
+            // 
+            // TBField
+            // 
+            this.TBField.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TBField.Location = new System.Drawing.Point(153, 182);
+            this.TBField.Name = "TBField";
+            this.TBField.ReadOnly = true;
+            this.TBField.Size = new System.Drawing.Size(170, 28);
+            this.TBField.TabIndex = 6;
+            // 
+            // comboBoxCameraTargetSurface
+            // 
+            this.comboBoxCameraTargetSurface.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCameraTargetSurface.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBoxCameraTargetSurface.FormattingEnabled = true;
+            this.comboBoxCameraTargetSurface.Location = new System.Drawing.Point(153, 11);
+            this.comboBoxCameraTargetSurface.Name = "comboBoxCameraTargetSurface";
+            this.comboBoxCameraTargetSurface.Size = new System.Drawing.Size(267, 26);
+            this.comboBoxCameraTargetSurface.TabIndex = 7;
+            // 
+            // comboBoxFaLens
+            // 
+            this.comboBoxFaLens.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFaLens.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBoxFaLens.FormattingEnabled = true;
+            this.comboBoxFaLens.Items.AddRange(new object[] {
+            "4",
+            "6",
+            "8",
+            "12",
+            "16",
+            "25",
+            "35",
+            "50",
+            "75"});
+            this.comboBoxFaLens.Location = new System.Drawing.Point(153, 65);
+            this.comboBoxFaLens.Name = "comboBoxFaLens";
+            this.comboBoxFaLens.Size = new System.Drawing.Size(170, 26);
+            this.comboBoxFaLens.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 13.2F);
+            this.label5.Location = new System.Drawing.Point(-8, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(692, 22);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "视场（FOV）=工作距离（WD）*CCD靶面型号尺寸（V或者H）/焦距（f）";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 15.8F);
+            this.button1.Location = new System.Drawing.Point(364, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 123);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Enter += new System.EventHandler(this.button1_Enter);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.linkLabel1.Location = new System.Drawing.Point(260, 263);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(102, 23);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "源码下载";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 298);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxFaLens);
+            this.Controls.Add(this.comboBoxCameraTargetSurface);
+            this.Controls.Add(this.TBField);
+            this.Controls.Add(this.TBDistance);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fa镜头计算视野软件";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TBDistance;
+        private System.Windows.Forms.TextBox TBField;
+        private System.Windows.Forms.ComboBox comboBoxCameraTargetSurface;
+        private System.Windows.Forms.ComboBox comboBoxFaLens;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fa镜头视野计算
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
